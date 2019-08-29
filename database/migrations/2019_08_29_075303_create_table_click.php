894263d37e7741313e919ce27ecc69c8456ffaa8
fly <1178097548@qq.com>
2019-08-29 15:53:38 +0800

    click

--- database/migrations/2019_08_29_075303_create_table_click.php
+++ database/migrations/2019_08_29_075303_create_table_click.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableUser extends Migration
+class CreateTableClick extends Migration
 {
     /**
      * Run the migrations.
